c0b5279f79c4172c7bfa1fa05dade0ac63637ce5
wenweihu86 <wenweihu86@163.com>
2018-06-18 12:29:37 +0800

    修改奖励为9.5以太

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -38,8 +38,8 @@ import (
 var (
 	//FrontierBlockReward    *big.Int = big.NewInt(5e+18) // Block reward in wei for successfully mining a block
 	//ByzantiumBlockReward   *big.Int = big.NewInt(3e+18) // Block reward in wei for successfully mining a block upward from Byzantium
-	FrontierBlockReward    *big.Int = big.NewInt(0.95e+18) // Block reward in wei for successfully mining a block
-	ByzantiumBlockReward   *big.Int = big.NewInt(0.95e+18) // Block reward in wei for successfully mining a block upward from Byzantium
+	FrontierBlockReward    *big.Int = big.NewInt(9.51e+18) // Block reward in wei for successfully mining a block
+	ByzantiumBlockReward   *big.Int = big.NewInt(9.51e+18) // Block reward in wei for successfully mining a block upward from Byzantium
 	maxUncles                       = 2                 // Maximum number of uncles allowed in a single block
 	allowedFutureBlockTime          = 15 * time.Second  // Max time from current time allowed for blocks, before they're considered future blocks
 )
